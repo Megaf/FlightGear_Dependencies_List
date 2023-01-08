27d5d8ae01ecfd258f7e9d4400a9dbeca7922e5f
Megaf <mmegaf@gmail.com>
2023-01-08 22:04:02 +0000

    Minimal list to get plib built.

--- flightgear_dependencies_list-debian.sh
+++ flightgear_dependencies_list-debian.sh
@@ -2,7 +2,8 @@
 
 # Dependencies for Debian Linux
 local basic_dependencies=(git subversion cmake build-essential ccache)
-local minimal_dependencies=(libopenal-dev libfreetype-dev libjpeg-dev libpng-dev libtiff-dev libxmu-dev libxi-dev zlib1g-dev )
+local plib_dependencies=(aclocal automake autoconf autogen)
+local minimal_dependencies=(libxxf86vm-dev libx11-dev libglu1-mesa-dev libopenal-dev libfreetype-dev libjpeg-dev libpng-dev libtiff-dev libxmu-dev libxi-dev zlib1g-dev )
 local extra_dependencies=(libgstreamer1.0-dev libqt5gstreamer-dev)
 
-sudo apt install -y ${basic_dependencies[@]} ${minimal_dependencies[@]}
+sudo apt install -y ${basic_dependencies[@]} ${plib_dependencies[@]} ${minimal_dependencies[@]}
