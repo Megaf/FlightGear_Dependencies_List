9a94d44ef2d393066a20e5230816e899c8190a73
Megaf <mmegaf@gmail.com>
2023-01-09 14:22:54 +0000

    Full dependencie list for Debian, without Launcher

--- flightgear_dependencies_list-debian.sh
+++ flightgear_dependencies_list-debian.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Dependencies for Debian Linux
+# Compilers and Code download
 local basic_dependencies=(git subversion cmake build-essential ccache)
+# PLIB
 local plib_dependencies=(aclocal automake autoconf autogen)
+# Minimal Depdencies to compiler OpenSceneGraph, SimGear and FlightGear. No QT Launcher.
 local minimal_dependencies=(libxxf86vm-dev libx11-dev libglu1-mesa-dev libopenal-dev libboost-dev libopenal-dev zlib1g-dev liblzma-dev libcurl4-gnutls-dev libfreetype-dev libjpeg-dev libpng-dev libtiff-dev libxmu-dev libxi-dev zlib1g-dev )
-local extra_dependencies=(libgstreamer1.0-dev libqt5gstreamer-dev)
+# QT Launcher
+#TODO
+#local qt_launcher_dependencies=()
 
+# Install dependencies
 sudo apt install -y ${basic_dependencies[@]} ${plib_dependencies[@]} ${minimal_dependencies[@]}
