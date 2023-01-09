d1d1b19cd9ca7b04c9b5d8dc8796f5194ff9a635
Megaf <mmegaf@gmail.com>
2023-01-09 15:46:45 +0000

    Removed aclocal from deps, provided by autogen
    Removed extra space

--- flightgear_dependencies_list-debian.sh
+++ flightgear_dependencies_list-debian.sh
@@ -4,9 +4,9 @@
 # Compilers and Code download
 local basic_dependencies=(git subversion cmake build-essential ccache)
 # PLIB
-local plib_dependencies=(aclocal automake autoconf autogen)
+local plib_dependencies=(automake autoconf autogen)
 # Minimal Depdencies to compiler OpenSceneGraph, SimGear and FlightGear. No QT Launcher.
-local minimal_dependencies=(libxxf86vm-dev libx11-dev libglu1-mesa-dev libopenal-dev libboost-dev libopenal-dev zlib1g-dev liblzma-dev libcurl4-gnutls-dev libfreetype-dev libjpeg-dev libpng-dev libtiff-dev libxmu-dev libxi-dev zlib1g-dev )
+local minimal_dependencies=(libxxf86vm-dev libx11-dev libglu1-mesa-dev libopenal-dev libboost-dev libopenal-dev zlib1g-dev liblzma-dev libcurl4-gnutls-dev libfreetype-dev libjpeg-dev libpng-dev libtiff-dev libxmu-dev libxi-dev zlib1g-dev)
 # QT Launcher
 #TODO
 #local qt_launcher_dependencies=()
